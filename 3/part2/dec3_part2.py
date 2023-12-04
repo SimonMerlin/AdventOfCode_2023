0be1172862dddb3fb8b09b8f42f1eeca527d50cd
merlin <merlinsimon98@gmail.com>
2023-12-04 07:17:44 +0100

    add day 3

--- 3/part2/dec3_part2.py
+++ 3/part2/dec3_part2.py
@@ -7,16 +7,19 @@ xMax, yMax = len(lines[0]), len(lines)
 
 
 def findFullNumber(x, y):
+    c = [(x, y)]
     number = lines[y][x]
     x1 = x
     while x1-1>=0 and lines[y][x1-1].isdigit():
         number = lines[y][x1-1] + number
+        c.append((x1-1, y))
         x1 -=1
     x2 = x
     while x2+1<xMax and lines[y][x2+1].isdigit():
         number = number + lines[y][x2+1]
+        c.append((x2+1, y))
         x2 +=1
-    return number
+    return number, c
 
 def getCoordinatesAround(x1, x2, y):
     coords = [(x1-1, y-1), (x1-1, y), (x1-1, y+1), (x2+1, y-1), (x2+1, y), (x2+1, y+1)]
@@ -31,13 +34,14 @@ def main():
         for x in range(xMax):
             if lines[y][x] == '*':
                 coords = getCoordinatesAround(x, x, y)
-                numbers = set()
+                numbers = list()
+                csUsed = []
                 for c in coords:
-                    if lines[c[1]][c[0]].isdigit():
-                        numbers.add(findFullNumber(c[0], c[1]))
-                print(numbers)
+                    if lines[c[1]][c[0]].isdigit() and not (c in csUsed):
+                        n, cUsed = findFullNumber(c[0], c[1])
+                        csUsed += cUsed
+                        numbers.append(n)
                 if len(numbers) == 2:
-                    numbers = list(numbers)
                     print(int(numbers[0]), int(numbers[1]))
                     counter += int(numbers[0]) * int(numbers[1])
     return counter
