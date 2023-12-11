0e153dd5002241819d287dfb40b7701907470b1c
SimonM <smerlin@unis.fr>
2023-12-11 13:11:48 +0100

    add day 11

---
+++ 11/part1/dec11_part1.py
@@ -0,0 +1,36 @@
+import sys
+import os
+import time
+f = open(os.path.join(sys.path[0], './../input.txt'), 'r')
+lines = [l.rstrip() for l in f.readlines()]
+
+def expandUniverse():
+    universe = []
+    for r in lines:
+        universe.append(r)
+        if all([s=='.' for s in r]):
+            universe.append(r)
+    cAdded = 0
+    for c in range(len(lines[0])):
+        if all([r[c]=='.' for r in lines]):
+            for i in range(len(universe)):
+                universe[i] = universe[i][:c+cAdded] + '.' + universe[i][c+cAdded:]
+            cAdded+=1
+    return universe
+
+def getGalaxies(universe):
+    return [(x, y) for y in range(len(universe)) for x in range(len(universe[y])) if universe[y][x] == '#']
+
+def getDistancesBetweenGalaxies(galaxies):
+    return sum([abs(galaxies[i][0] - galaxies[j+i][0]) + abs(galaxies[i][1] - galaxies[j+i][1]) for i in range(len(galaxies)) for j in range(len(galaxies)-i)])
+
+def main():
+    global lines
+    universe = expandUniverse()
+    galaxies = getGalaxies(universe)
+    return getDistancesBetweenGalaxies(galaxies)
+
+start = time.perf_counter()
+print(main())
+end = time.perf_counter()
+print(f"Executed in {((end - start)*1000):0.2f} milliseconds")
---
+++ 11/part2/dec11_part2.py
@@ -0,0 +1,41 @@
+import sys
+import os
+import time
+f = open(os.path.join(sys.path[0], './../input.txt'), 'r')
+universe = [l.rstrip() for l in f.readlines()]
+
+EXPANSION_RATIO = 1000000
+
+def getEmptyRows(universe):
+    return [r for r in range(len(universe)) if all([s=='.' for s in universe[r]])]
+
+def getEmptyColumns(universe):
+    return [c for c in range(len(universe[0])) if all([r[c]=='.' for r in universe])]
+
+def countEmptyBetween(a, b, list):
+    a, b = (a, b) if a<b else (b, a)
+    return len([e for e in list if e>a and e<b])
+
+def getGalaxies(universe):
+    return [(x, y) for y in range(len(universe)) for x in range(len(universe[y])) if universe[y][x] == '#']
+
+def getDistancesBetweenGalaxies(galaxies, emptyRows, emptyColumns):
+    cpt=0
+    for i in range(len(galaxies)):
+        for j in range(len(galaxies)-i):
+            countEmptyColumns = countEmptyBetween(galaxies[i][0], galaxies[j+i][0], emptyColumns)
+            countEmptyRows = countEmptyBetween(galaxies[i][1], galaxies[j+i][1], emptyRows)
+            deltaX = abs(galaxies[i][0] - galaxies[i+j][0]) - countEmptyColumns + countEmptyColumns*EXPANSION_RATIO
+            deltaY = abs(galaxies[i][1] - galaxies[i+j][1]) - countEmptyRows + countEmptyRows*EXPANSION_RATIO
+            cpt += deltaX + deltaY
+    return cpt
+
+def main():
+    emptyRows, emptyColumns = getEmptyRows(universe), getEmptyColumns(universe)
+    galaxies = getGalaxies(universe)
+    return getDistancesBetweenGalaxies(galaxies, emptyRows, emptyColumns)
+
+start = time.perf_counter()
+print(main())
+end = time.perf_counter()
+print(f"Executed in {((end - start)*1000):0.2f} milliseconds")
