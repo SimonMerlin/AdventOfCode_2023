6ee2e482d031d8e4ca597ff33334e9e68c14876e
SimonM <smerlin@unis.fr>
2023-12-18 08:38:21 +0100

    one more try day 12

--- 12/README.md
+++ 12/README.md
@@ -5,3 +5,10 @@
 
 # Part 2
 1145754067804 too low
+2550727822802 too low
+2396729724271 too low
+2553550079571 not the right answer
+2553358837099 not the right answer
+2550727822802 not the right answer
+2527082820610 not the right answer
+2529713834907 not the right answer
--- 12/part2/dec12_part2.py
+++ 12/part2/dec12_part2.py
@@ -27,15 +27,53 @@ def countAllowedPattern(s, wantedValue):
         if value == wantedValue:
             cpt+=1
     return cpt
+
+def countAllowedPatternV2(s, wantedValue):
+    cpt = 0
+    seq = list(s)
+
+    # Find indices of char to replace
+    indices = [i for i, c in enumerate(seq) if c=='?']
+    #print("? = ", len(indices), 2**len(indices))
+    # Generate key letter combinations & place them into the list
+    #stats = {'d#': 0, 'd.': 0, 'f#': 0, 'f.': 0}
+    for t in product('.#', repeat=len(indices)):
+        for i, c in zip(indices, t):
+            seq[i] = c
+        newArr = ''.join(seq)
+        value = getArrangementValue(newArr)
+        if value == wantedValue:
+            cpt+=1
+    #print(stats)
+
+    return cpt
     
 
 def countArrangements(s, wantedValue, unfoldRatio):
-    value = countAllowedPattern(s, wantedValue)
-    #print("Initial : ", value)
+    value = countAllowedPatternV2(s, wantedValue)
+    print("Initial : ", value)
+    print()
+
+    nextValue = countAllowedPatternV2(s+'.', wantedValue)
+    print(". fin : ", nextValue)
+    print()
+
+    nextValue = countAllowedPatternV2(s+'#', wantedValue)
+    print("# fin : ", nextValue)
+    print()
+
+    nextValue = countAllowedPatternV2('.'+s, wantedValue)
+    print(". debut : ", nextValue)
+    print()
+
+    nextValue = countAllowedPatternV2('#'+s, wantedValue)
+    print("# debut : ", nextValue)
+    print()
 
     #get value for twice as big
-    nextValue = countAllowedPattern(s+'?'+s, wantedValue+wantedValue)
-    #print("Unfold 2 : ", nextValue)
+    nextValue = countAllowedPatternV2(s+'?'+s, wantedValue+wantedValue)
+    print("Unfold 2 : ", nextValue)
+    print()
 
     #si se termine par . => after ; sinon before
     #growthRatio = 1
@@ -63,22 +101,59 @@ def countArrangements(s, wantedValue, unfoldRatio):
     #print()
     return int(value)
 
+def countArrangementsV2(s, wantedValue, unfoldRatio):
+    # get stats
+    stats = {'d#': 0, 'd.': 0, 'f#': 0, 'f.': 0}
+    stats['f.'] = countAllowedPatternV2(s+'.', wantedValue)
+    stats['f#'] = countAllowedPatternV2(s+'#', wantedValue)
+    stats['d.'] = countAllowedPatternV2('.'+s, wantedValue)
+    stats['d#'] = countAllowedPatternV2('#'+s, wantedValue)
+    value = countAllowedPatternV2(s, wantedValue)
+    lastWantedValue = wantedValue[-1]
+    lastChars = s[-lastWantedValue:]
+
+    print(stats)
+    
+    #if s[-1]=='#' and all(x in {'?', '#'} for x in lastChars) and (s[-1-len(lastChars)]=='.' or s[-1-len(lastChars)]=='?'):
+        #nextValue = value*value
+    #else:
+        #nextValue = (stats['d#'] + stats['d.']) * (stats['f#'] + stats['f.'])
+    
+    # end by .
+    if s[-1]=='.': 
+        nextValue = value*(stats['d#'] + stats['d.'])
+    # last chars are #
+    elif s[-1]=='#' and all(x in {'?', '#'} for x in lastChars) and s[-1-len(lastChars)]=='.':
+        nextValue = value*value
+    else:
+        nextValue = (stats['d#'] + stats['d.']) * (stats['f#'] + stats['f.'])
+
+    #get growth ratio
+    growthRatio = nextValue/value
+    #print("Growth ratio : ", int(growthRatio))
+
+    #get value for unfoldRatio
+    for _ in range(unfoldRatio-1):
+        value *= growthRatio
+    print(int(value))
+    print()
+    return int(value)
+
 
 def main(procnum, list, resultDict):
     s=0
     print("Proc#{}".format(procnum))
     for i, l in enumerate(list):
-        print("Proc#{} : {}".format(procnum, i))
+        print(i)
         key, val = l.split(' ')
         val = [int(v) for v in val.split(',')]
-        s += sum([countArrangements(key, val, 5)])
+        s += sum([countArrangementsV2(key, val, 5)])
     print("Proc#{} : {}".format(procnum, s))
     resultDict[procnum] = s
 
 
-#lines[i*(len(lines)/nbProcess):(i+1)*(len(lines)/nbProcess)], 
 def mainMultiprocess():
-    nbProcess = 10
+    nbProcess = 1
     jobs = []
     manager = multiprocessing.Manager()
     resultDict = manager.dict()
